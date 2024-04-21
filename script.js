cfa453dc3017b71a2c2cc8954d5e1faa4f35f232
kurokitabc <161806557+kurokitabc@users.noreply.github.com>
2024-04-21 09:40:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ window.addEventListener('scroll', function() {
   var bodyHeight = document.body.offsetHeight;
   var scrollPercentage = (scrollPosition / (bodyHeight - windowHeight)) * 100;
   document.getElementById('percentage').innerText = Math.round(scrollPercentage) + "%";
+
+  if (scrollPercentage >= 100) {
+    document.getElementById('scrollText').innerText = "Welcome to KBCLab!!";
+  } else {
+    document.getElementById('scrollText').innerText = "スクロールしてください";
+  }
 });
